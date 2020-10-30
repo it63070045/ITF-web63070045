ca08bf9ccd3d1e47c73523dd3ef113b3dbf8d1e3
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-10-30 12:44:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>My Work</h1>
+        <p>NATANON  HANCHANA    BIG</p>
+        <img src="make.jpg">
+        <br>
+        <img src="namo.jpg">
+        <br>
+        <a href="https://www.amazon.com/">This is a Link</a>
+        <br>
+        <h2>Joke</h2>
+        <p>ช : เราเลิกกันเถอะผมรู้ว่าคุณรักผมน้อยลง</p>
+        <p>ญ : ฉันไม่เคยรักคุณน้อยลงนะ</p>
+        <p>ช : น้อยสิ เดือนก่อนคุณรักผม 31 วัน เดือนนี้คุณรักผม 30 วัน</p>
+        </ul>
+
+    </body>
+</html>
