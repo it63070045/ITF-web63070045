a635e38177e9a2c8dad3d00959731578929f97f0
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-11-06 22:47:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h1>My Work</h1>
-        <p>NATANON  HANCHANA    BIG</p>
-        <img src="make.jpg">
-        <br>
-        <img src="namo.jpg">
-        <br>
-        <a href="https://www.amazon.com/">This is a Link</a>
-        <br>
-        <h2>Joke</h2>
-        <p>ช : เราเลิกกันเถอะผมรู้ว่าคุณรักผมน้อยลง</p>
-        <p>ญ : ฉันไม่เคยรักคุณน้อยลงนะ</p>
-        <p>ช : น้อยสิ เดือนก่อนคุณรักผม 31 วัน เดือนนี้คุณรักผม 30 วัน</p>
-        </ul>
-
-    </body>
+<head>
+    <title>LAB ITF</title>
+</head>
+<body>
+    <h3>Work2 / Work3 06/11/2020</h3>
+    <a href="work itf/work1">work1</a><br>
+    <a href="work itf/work2">work2</a>
+</body>
 </html>
